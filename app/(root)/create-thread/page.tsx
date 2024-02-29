148e4d7167c15e95a8338250bd4c0ebd5f70abea
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2024-02-29 14:33:38 +0200

    feat: add create thread page

---
+++ app/(root)/create-thread/page.tsx
@@ -0,0 +1,22 @@
+
+import PostThread from "@/components/forms/PostThread";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+
+async function Page(){
+    
+    const user = await currentUser();
+    if(!user) return null;
+    const userInfo =await fetchUser(user.id);
+    if(!userInfo?.onboarded) redirect('/onboarding');
+
+    return(
+        <div>
+          <h1 className ='head-text'>Create Thread</h1>
+          <PostThread userId={userInfo._id}/>
+        </div>
+    )
+}
+export default Page;
