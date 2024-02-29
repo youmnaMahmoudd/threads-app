73964af0729fd37ea34dfafb26b2d21800a45df3
youmnaMahmoudd <104006222+youmnaMahmoudd@users.noreply.github.com>
2024-02-29 14:38:53 +0200

    feat: Add user-related functions and utilities

---
+++ lib/models/user.model.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: String,
+  bio: String,
+  threads: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thread",
+    },
+  ],
+  onboarded: {
+    type: Boolean,
+    default: false,
+  },
+  communities: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Community",
+    },
+  ],
+});
+//For the first time mongoose model is not existing yet so it will fall back in creating a mongoose model user 
+//based on user schema but starting from the second time it is going to know to create it off of that instance 
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
